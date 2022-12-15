1ba510fbc56ffc7ade6d1800980b183a544c7f31
Archer226 <ffff69475@gmail.com>
2022-12-15 11:01:37 +0200

    Hidden Search Widget Day-04

---
+++ js/script.js
@@ -0,0 +1,6 @@
+let searchIcon = document.getElementById("searchicon");
+
+searchIcon.addEventListener("click", () => {
+  let searchBar = document.getElementById("search");
+  searchBar.classList.toggle("active");
+});
